ffb6cd8de5be1188d4f6b842955d34e634d4c29b
James Dickson <jamesstuartdickson@gmail.com>
2023-07-07 13:23:49 +0100

    updated advanced_logic_exercise.py (previously copied over users_exercise.py to it

--- advanced_logic_exercise.py
+++ advanced_logic_exercise.py
@@ -1,66 +1,25 @@
-users = {
-  "Jonathan": {
-    "twitter": "jonnyt",
-    "lottery_numbers": [6, 12, 49, 33, 45, 20],
-    "home_town": "Stirling",
-    "pets": [
-    {
-      "name": "fluffy",
-      "species": "cat"
-    },
-    {
-      "name": "fido",
-      "species": "dog"
-    },
-    {
-      "name": "spike",
-      "species": "dog"
-    }
-  ]
-  },
-  "Erik": {
-    "twitter": "eriksf",
-    "lottery_numbers": [18, 34, 8, 11, 24],
-    "home_town": "Linlithgow",
-    "pets": [
-    {
-      "name": "nemo",
-      "species": "fish"
-    },
-    {
-      "name": "kevin",
-      "species": "fish"
-    },
-    {
-      "name": "spike",
-      "species": "dog"
-    },
-    {
-      "name": "rupert",
-      "species": "parrot"
-    }
-  ]
-  },
-  "Avril": {
-    "twitter": "bridgpally",
-    "lottery_numbers": [12, 14, 33, 38, 9, 25],
-    "home_town": "Dunbar",
-    "pets": [
-      {
-        "name": "monty",
-        "species": "snake"
-      }
-    ]
-  }
-}
+# For the following list of numbers:
 
-# 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-# 2. Get Erik's hometown
-# 3. Get the list of Erik's lottery numbers
-# 4. Get the species of Avril's pet Monty
-# 5. Get the smallest of Erik's lottery numbers
-# 6. Return an list of Avril's lottery numbers that are even
-# 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
-# 8. Change Erik's hometown to Edinburgh
-# 9. Add a pet dog to Erik called "fluffy"
-# 10. Add another person to the users dictionary
+numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
+
+# 1. Print out a list of the even integers:
+
+
+# 2. Print the difference between the largest and smallest value:
+
+
+# 3. Print True if the list contains a 2 next to a 2 somewhere.
+
+
+# 4. Print the sum of the numbers, 
+#    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
+#    
+#    So [11, 6, 4, 99, 7, 11] would have sum of 22
+
+
+# 5. HARD! Print the sum of the numbers. 
+#    Except the number 13 is very unlucky, so it does not count.
+#    And numbers that come immediately after a 13 also do not count.
+#    HINT - You will need to track the index throughout the loop.
+#
+#    So [5, 13, 2] would have sum of 5. 
